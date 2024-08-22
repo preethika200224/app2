ef400a73ccca57e283440b30eda1f19805bb9bd6
preethika200224 <preethikareddys82@gmail.com>
2024-08-22 21:23:17 +0530

    Add files via upload

---
+++ QuestionAnswer.java
@@ -0,0 +1,26 @@
+package easy.tuto.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "For which of the following disciplines is Nobel Prize awarded?",
+            "Which one is not the programming language?",
+            "Which is the biggest continent in the world?",
+            "Which is the longest river in the world?"
+    };
+
+    public static String choices[][] = {
+            {"Physics and Chemistry","Physiology or Medicine", "Literature, Peace and Economics", "All of the above"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"North America","Asia","Africa","Australia"},
+            {"Great Ganga","Nile","Amazon", "Niger"}
+    };
+
+    public static String correctAnswers[] = {
+            "All of the above",
+            "Notepad",
+            "Asia",
+            "Nile"
+    };
+
+}
